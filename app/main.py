87ff0b52cfc6bf0482a6d7717b0b5afa276da42f
Ahmed mokhtar <ahmedmokhar2407@gmail.com>
2024-12-17 23:33:21 +0200

    second commit

--- app/main.py
+++ app/main.py
@@ -6,13 +6,13 @@ load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('<random_string>')
+@app.route('/<random_string>')
 def return_backwards_string(random_string):
     return "".join(reversed(random_string))
-
+# comment
 @app.route('/get-mode')
 def get_mode():
     return os.environ.get("MODE")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8000)
